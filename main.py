aac65cff84c886418f8ad53bbdc337ed0eedcd13
Gabriel Pasqualini <gabrielpasqualini2@gmail.com>
2021-12-01 23:08:53 -0300

    faq commit

--- main.py
+++ main.py
@@ -2,8 +2,7 @@ import json
 from os import times
 from types import ModuleType
 import time
-from banco import cadastro, conferirr, desmarcarr, find_user, horariosDisp, setHorario
-#from database import *
+from banco import cadastro, conferirr, convenios, desmarcarr, find_user, horariosDisp, setHorario
 from chatterbot import ChatBot
 import telebot
 
@@ -14,22 +13,21 @@ bot = telebot.TeleBot(token)
 print('')
 print('|================================|Bot on|================================|')
 
-
 @bot.message_handler(commands=["agendar"])
 def agendar(msg):
-    bot.send_message(msg.chat.id, 'Estes são os horarios disponiveis:')
     # constants from telegram
     fname = msg.from_user.first_name
     lname = msg.from_user.last_name
     idTele = msg.from_user.id
     try:
-        
         user = find_user(idTele)
         if len(user) <= 0:
         
             cadastro(idTele, lname, fname)
 
         t = horariosDisp()
+        if len(t)<= 0:
+            bot.send_message(msg.chat.id, f'Desculpe, estamos sem horários disponíveis.')
         
         for i in range(len(t)):
             
@@ -48,6 +46,7 @@ def agendar(msg):
            
             bot.send_message(
                 msg.chat.id, f'/Dia {dia} do mes {mes} no ano {ano} Horario {hora} e {minuto}'.replace("-", "_").replace(" ", "_"))
+                                
     except Exception as err:
         print(err)
 
@@ -57,7 +56,7 @@ def conferir(msg):
     t = conferirr(idTele)
     
     if len(t) <=0:
-        bot.send_message(msg.chat.id, f'Voce não tem consulta agendada, para agendar clique aqui -> /agendar')
+        bot.send_message(msg.chat.id, f'Você não tem consulta agendada, para agendar clique aqui -> /agendar')
    
     for i in range(len(t)):
         data = str(t[i][4])
@@ -65,14 +64,31 @@ def conferir(msg):
         mes =data[5]+data[6]
         hora = data[11]+data[12]
         minuto = data[14]+data[15]
-        bot.send_message(msg.chat.id, f'Voce tem consulta para o dia {dia} do mes {mes} as {hora}:{minuto}')
-
+        bot.send_message(msg.chat.id, f'Você tem consulta para o dia {dia} do mês {mes} às {hora}:{minuto}')
 
 @bot.message_handler(commands=["desmarcar"])
 def desmarcar(msg):
     idTele = msg.from_user.id
-    var = desmarcarr(idTele)
-    bot.send_message(msg.chat.id, var)
+    num = conferirr(idTele)
+    if len(num) >= 1:
+        idTele = msg.from_user.id
+        var = desmarcarr(idTele)
+        bot.send_message(msg.chat.id, var)
+    else:
+        bot.send_message(msg.chat.id, "Você não tem consulta para desmarcar")
+
+@bot.message_handler(commands=["duvidas"])
+def duvidas(msg):
+    bot.send_message(msg.chat.id, "/D1 -> Como saber se o meu convênio é aceito pela Clínica Médica?")
+    bot.send_message(msg.chat.id, "/D2 -> Existem estacionamentos próximos à Clínica Médica?")
+    bot.send_message(msg.chat.id, "/D3 -> Como chegar as unidades da Clínica Médica")
+    
+@bot.message_handler(commands=["lista"])
+def lista(msg):
+    conv = convenios()  
+    bot.send_message(msg.chat.id,"Segue a nossa lista de convênios:")
+    for i in range(len(conv)):  
+        bot.send_message(msg.chat.id,conv[i])
 
 def verify(msg):
     return True
@@ -82,29 +98,52 @@ def default(msg):
     name = msg.from_user.first_name
     idTele = msg.from_user.id
     texto = str(msg.text)
-
-    if texto.startswith('/Dia'):
-        print(texto)
-        dia = texto[5]+texto[6]
-        ano = texto[25]+texto[26]+texto[27]+texto[28]
-        mes = texto[15]+texto[16]
-        hora = texto[38]+texto[39]+':'+texto[43]+texto[44]+':00'
-        print(dia+'-'+mes+'-'+ano+' '+hora)
-        marcar = (ano+'-'+mes+'-'+dia+' '+hora)
-       
-        setHorario(name,idTele,marcar)
-
-        bot.send_message(msg.chat.id, 'Vou marcar para o dia em questão')
-
-    else:
-        opcoes = f'''
-        Ola {name}, o que deseja? 
-/agendar => Aqui é onde agenda a consulta. 
-/conferir => Aqui é onde voce revisa as sua consulta.
-/desmarcar => Aqui é onde voce desmarcar a consulta. 
+    textoL = texto.lower()
+    opcoes = f'''
+/agendar => Agendo sua consulta nos dias disponíveis. 
+/conferir => Te mostro a data e horário de sua consulta agendada.
+/desmarcar => Desmarco sua consulta. 
+/duvidas => Dúvidas frequentes.
         '''
 
-        bot.send_message(msg.chat.id, opcoes)
+    if texto.startswith('/Dia'):
+        num = conferirr(idTele)
+        if len(num) == 0:
+            print(texto)
+            dia = texto[5]+texto[6]
+            ano = texto[25]+texto[26]+texto[27]+texto[28]
+            mes = texto[15]+texto[16]
+            hora = texto[38]+texto[39]+':'+texto[43]+texto[44]+':00'
+            print(dia+'-'+mes+'-'+ano+' '+hora)
+            marcar = (ano+'-'+mes+'-'+dia+' '+hora)
+       
+            hora2 = texto[38]+texto[39]+':'+texto[43]+texto[44]
+            setHorario(name,idTele,marcar) #ENVIO AO BANCO DE DADOS
+            bot.send_message(msg.chat.id, 'Vou marcar para o dia em questão')
+            time.sleep(1)
+            return bot.send_message(msg.chat.id, f'Feito, está marcado para o dia {dia} do mes {mes} as {hora2}')
+        
+        return bot.send_message(msg.chat.id, 'Voce ja tem consulta marcada')    
+    
+    if textoL.startswith('oi') or textoL.startswith('ola') or textoL.startswith('bom dia') or textoL.startswith('boa tarde') or textoL.startswith('boa noite'):        
+        
+        bot.send_message(msg.chat.id, f"Olá {name}, em que posso ser útil?")
+        return bot.send_message(msg.chat.id, opcoes)
 
+    if texto.startswith('/D'):
+        op = texto.split('D')[1]
+        if op == '1':
+            return bot.send_message(msg.chat.id, "Verifique nossa /lista de convênios médicos.")
+
+        if op == '2':
+            return bot.send_message(msg.chat.id, "Sim, a clínica possui estacionamento próprio.")
+
+        if op == '3':
+            return bot.send_message(msg.chat.id, "O endereço da unidade é a Rua das Amoras, 1456 Jardim das flores")    
+
+        return bot.send_message(msg.chat.id, "Dúvida não disponível") 
+   
+    bot.send_message(msg.chat.id, "Desculpe, não entendi o que disse...")
+    return bot.send_message(msg.chat.id, opcoes)
 
 bot.polling()
